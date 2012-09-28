2d78906d4de6877f5ab847cef137a0b1d3f88485
unknown <jeff.massung@VOLI31236.thqinc.com>
2012-09-28 16:41:05 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,214 @@
+# The LEXER Package
+
+The `LEXER` package is a very simple, small, and fast regular expression library for [LispWorks](http://www.lispworks.com). It follows the pattern matching syntax used by [Lua](http://www.lua.org) for simple regular expression functionality (match, searching, splitting, and replacing). You can see the syntax for these patterns [here](http://www.lua.org/pil/20.2.html).
+
+It also has a lexer macro that tokenizes string and is designed to be used in conjuction with the [`PARSERGEN`](http://www.lispworks.com/documentation/lw50/LWRM/html/lwref-433.htm) package built into [LispWorks](http://www.lispworks.com).
+
+# Lua-style Regular Expressions
+
+The two exposed classes are `re` and `re-match`. The `re` class doesn't expose any functionality, but the `re-match` class exposes the following reader functions:
+
+	#'match-string
+	#'match-captures
+	#'match-pos-start
+	#'match-pos-end
+
+# Creating a Regular Expression
+
+To create a `re` object, you can either use the `compile-re` function or the `#/` dispatch macro.
+
+	CL-USER 1 > (compile-re "%d+")
+	#<RE "%d+">
+
+	CL-USER 2 > #/%d+/
+	#<RE "%d+">
+
+The `compile-re` function allows you to pass some extra options to the regular expression.
+
+	(compile-re pattern &key case-fold multi-line)
+
+If `case-fold` is non-nil then pattern matching on that expression will be case-insensitive. If `multi-line` is non-nil then the `$` (end of input) pattern will match newlines and the `.` pattern will not match newlines.
+
+*Hint: when using the `#/` character dispatch macro, use `\` (backslash) to escape the `/` and other characters that might mess with LispWorks's syntax coloring.*
+
+# Pattern Matching
+
+Once you have a regular expression object, you can send it off to one of the pattern matching functions:
+
+	(match-re re string &key start end exact)
+	(find-re re string &key start end all)
+	(split-re re string &key start end all coalesce-seps)
+	(replace-re re with string &key start end all)
+
+The `match-re` function attempts to match (from `start`) the expression exactly as it is. If `exact` is non-nil then `match-re` will return `nil` if the pattern doesn't match to the end of the string. It returns a `re-match` if the pattern matches.
+
+The `find-re` function will scan through the string and look for any match to the pattern. If `all` is non-nil, then it will return a list of all matches found, otherwise it simply returns the first match (or `nil`).
+
+The `split-re` function will find a match and then split the input string at the match. If `all` is nil, then 2 values are returned: the `subseq` left of the match and the `subseq` to the right of the match. If `all` is non-nil, then a list of subsequences are returned. If `coalesce-seps` is non-nil then any subsequence that is an empty string will be left out of the results. `coalesce-seps` is ignored if `all` is `nil`. If `all` is `nil` and the separator pattern is not found, then just a single value (the original string) is returned.
+
+The `replace-re` function searches for the pattern within the string and then calls the `with` function, passing it the `re-match` found. The `with` function then returns a new string to replace. If `all` is non-nil, then all patterns within `string` are replaced.
+
+# Examples
+
+	CL-USER > (match-re #/%d+/ "abc 123")
+	NIL
+
+	CL-USER > (match-re #/%d+/ "123 abc" :exact t)
+	NIL
+
+	CL-USER > (match-re #/%d+/ "123 abc")
+	#<RE-MATCH "123">
+
+	CL-USER > (match-pos-end *)
+	3
+
+	CL-USER > (find-re #/%d+/ "abc 123 def")
+	#<RE-MATCH "123">
+
+	CL-USER > (find-re #/%d+/ "123 4 5678" :all t)
+	(#<RE-MATCH "123"> #<RE-MATCH "4"> #<RE-MATCH "5678">)
+
+	CL-USER > (split-re #/,/ "1,2,3")
+	"1"
+	"2,3"
+
+	CL-USER > (split-re #/,/ "1 2 3")
+	"1 2 3"
+
+	CL-USER > (split-re #/,/ "1,2,3" :all t)
+	("1" "2" "3")
+
+	CL-USER > (split-re #/,/ "1,,,,2,,3" :all t)
+	("1" "" "" "" "2" "" "3")
+
+	CL-USER > (split-re #/,/ "1,,,,2,,3" :all t :coalesce-seps t)
+	("1" "2" "3")
+
+	CL-USER > (replace-re #/%d/ #'(lambda (m) "*") "1 2 3")
+	"* 2 3"
+
+	CL-USER > (replace-re #/%d/ #'(lambda (m) "*") "1 2 3" :all t)
+	"* * *"
+
+# The `with-re-match` Macro
+
+Once you have a `re-match` object, while you can get at the captures and matched string yourself, the `with-re-match` macro is very helpful in letting you use that information in a more readable manner.
+
+	(with-re-match ((var match) &body body)
+
+The `var` is the symbol that will be lexically bound to the results of `match`. Once inside `body`, however, There will be 10 additional symbols: `$$`, `$1`, `$2`, `$3`, ... `$9`. These are bound to the `match-string` and the `match-capture` strings.
+
+	CL-USER > (with-re-match (m (find-re #/{([^}]+)}/ "this {is a} test"))
+	            (print $$)
+	            (print $1))
+	"{is a}"
+	"is a"
+	"is a"
+	T
+
+The first two results are the `print` outputs. `$$` was bound to the `match-string` of the match and `$1` was bound to the first captured submatch.
+
+The last two results are the multiple return values of `body` and `T` indicates that the match was non-nil. Had `find-re` returned `nil`, then `body` would not have executed and `nil` would have been returned. This distinguishes itself from when `body` returns `nil`, but the match was found.
+
+	CL-USER > (flet ((initial (m)
+	                   (with-re-match (v m)
+	                     (format nil "~a." $1))))
+	            (replace-re #/(%a)%a+%s*/ #'initial "Lisp In Small Pieces" :all t))
+	"L.I.S.P."
+
+# Tokenizing With `deflexer`
+
+The pattern matching functionality provided - while very useful - is only a small part of what the `LEXER` package was designed for.
+
+[LispWorks](http://www.lispworks.com) comes with a fantastic [`PARSERGEN`](http://www.lispworks.com/documentation/lw50/LWRM/html/lwref-433.htm) package that - given a grammar - will create a function to parse a series of tokens (see the [`defparser`](http://www.lispworks.com/documentation/lw60/LW/html/lw-301.htm#pgfId-886013) function). 
+
+The `LEXER` package comes with a similar macro: `deflexer`. The `deflexer` macro is called with a set of token/body pairs and produces a function that - when handed a string - returns a new function that can be sent to a parser, which will then hand-off the next token parsed upon request.
+
+A simple example:
+
+	CL-USER > (deflexer my-lexer ()
+	            (#/%s+/)
+	            (#/%d+/   (values :number (parse-integer $$)))
+	            (#/%a%w*/ (values :ident $$))
+	            (#/=/     :eq))
+	MY-LEXER
+
+	CL-USER > (my-lexer " x = 10 ")
+	#<anonymous interpreted function 21B0FA8A>
+
+Each time the anonymous function is called, it will return the next token in the stream. Patterns that have no token body associated with them (e.g. the whitespace example above) are skipped. The token patterns are tried, in-order, so if there is any ambiguity the first one will win out (read: be careful!).
+
+Let's put the above lexer to some use by first creating a really simple grammar...
+
+	CL-USER > (defparser my-parser
+	            ((start let) $1)
+                ((let :ident :eq :number) (list :let $1 $3)))
+	MY-PARSER
+
+We can now send our lexer (with source) to the parser...
+
+	CL-USER > (my-parser (my-lexer "x = 10"))
+	(:let "x" 10)
+	NIL
+
+And done!
+
+# Error Handling
+
+While the tokenizer function produced by `deflexer` reads tokens from a string, it doesn't actually parse the entire string and then keep handing out tokens. Instead, it's parsing the tokens on-demand each call. For this reason, when all the token patterns fail, a `lex-error` condition will be signaled that specifies where in the source string (line # and at which exact character offset) things have gone wrong.
+
+	CL-USER > (my-lexer "!x = 10")
+	#<anonymous interpreted function 21CE915A>
+
+	CL-USER > (funcall *)
+
+	Error: Parse error on line 1 near '!'
+	  1 (abort) Return to level 0.
+	  2 Return to top loop level 0.
+
+Work is being added for additional restart options like editing the source, viewing the line the error occured on, skipping the character, etc.
+
+# How It Works
+
+Under-the-hood, each compiled pattern (`re` object) is actually a [Parsec](http://legacy.cs.uu.nl/daan/parsec.html)-like, monadic function that takes an input stream as its only argument. It then tracks whether the function returned `nil` (failed to match) or non-nil and how many characters from the stream were matched.
+
+As an example, the pattern `"%a%w*"` would compile to the following monadic function:
+
+	CL-USER > (lex::bind (lex::one-of lex::+letter+) (lex::many (lex::one-of lex::+alpha-numeric+)))
+	#<Closure 1 subfunction of LEXER::BIND 21AAA112>
+
+That closure can then be passed an input stream (wrapped in a state object):
+
+	CL-USER > (with-input-from-string (s "abcd1234")
+	            (funcall * (make-instance 'lex::lex-state :source s)))
+	T
+
+And we now see that the string `"abcd1234"` matches the pattern `"%a%w*"` and can look at the input stream to see how many characters were matched.
+
+There is a bit more to it than the above (captures, etc.) but the above shows the majority of what's going on under-the-hood. The code is not very complex... feel free to dive in and take a look!
+
+The `deflexer` macro builds a function that checks each of the token patterns in-order. The first one to succeed has its body executed and returned from the function. In pseudo-code, each call of the resulting function would look something like this:
+
+	(block next-token
+	  (tagbody
+	   skip-token
+	   (for-each pattern
+	     (when (match-re pattern source)
+           (if (null body)
+               (go skip-token)
+             (return-from next-token ,@body))))
+       (error token-fail)))
+
+# TODO List
+
+There are still a couple things that I want to do:
+
+* Add some good restarts to `lex-error`.
+* Range sets (e.g. `[a-z]`).
+* Multiple, exclusive sets (e.g. `[%P%D]`).
+
+None are especially hard, but they aren't a high priority at the moment. If one of these (or something else) is for you, let me know and I'll see what I can do.
+
+# Thank You!
+
+If you get some good use out of this package, please let me know; it's nice to know your work is valued by others. If you find a bug or made a nice addition (especially if you improved performance!) please feel free to drop me a line and let me know at [massung@gmail.com](mailto:massung@gmail.com).
---
+++ lexer.lisp
@@ -0,0 +1,409 @@
+;;;; lexer.lisp
+;;;;
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (require "parsergen"))
+
+(defpackage :lexer
+  (:use :cl :parsergen)
+  (:nicknames :lex)
+  (:export
+   #:re
+   #:re-match
+
+   ;; macros
+   #:with-re-match
+   #:deflexer
+
+   ;; pattern functions
+   #:compile-re
+   #:match-re
+   #:find-re
+   #:split-re
+   #:replace-re
+
+   ;; match functions
+   #:match-string
+   #:match-captures
+   #:match-pos-start
+   #:match-pos-end))
+
+(in-package :lexer)
+
+(defclass re ()
+  ((pattern    :initarg :pattern    :reader re-pattern)
+   (expression :initarg :expression :reader re-expression))
+  (:documentation "Regular expression."))
+
+(defclass re-match ()
+  ((match     :initarg :match     :reader match-string)
+   (captures  :initarg :captures  :reader match-captures)
+   (start-pos :initarg :start-pos :reader match-pos-start)
+   (end-pos   :initarg :end-pos   :reader match-pos-end))
+  (:documentation "Matched token."))
+
+(defclass lex-state ()
+  ((source   :initarg :source   :reader lex-source)
+   (captures :initarg :captures :reader lex-captures))
+  (:documentation "Token pattern matching state."))
+
+(define-condition lex-error (error)
+  ((source :initarg :source :reader lex-error-source)
+   (pos    :initarg :pos    :reader lex-error-pos)
+   (line   :initarg :line   :reader lex-error-line))
+  (:documentation "Error signaled when a deflexer fails all patterns.")
+  (:report (lambda (c s)
+             (with-slots (source pos line)
+                 c
+               (format s "Parse error on line ~d near '~c'" line (char source pos))))))
+
+(defmethod print-object ((re re) s)
+  "Output a regular expression to a stream."
+  (print-unreadable-object (re s :type t)
+    (format s "~s" (re-pattern re))))
+
+(defmethod print-object ((match re-match) s)
+  "Output a regular expression match to a stream."
+  (print-unreadable-object (match s :type t)
+    (format s "~s" (match-string match))))
+
+(defvar *case-fold* nil "Case-insentitive comparison.")
+(defvar *multi-line* nil "Dot and EOL also match newlines.")
+
+(defconstant +lowercase-letter+ "abcdefghijklmnopqrstuvwxyz")
+(defconstant +uppercase-letter+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+(defconstant +digit+ "0123456789")
+(defconstant +hex-digit+ "0123456789abcdefABCDEF")
+(defconstant +punctuation+ "`~!@#$%^&*()-+=[]{}\|;:',./<>?\"")
+(defconstant +letter+ #.(concatenate 'string +lowercase-letter+ +uppercase-letter+))
+(defconstant +alpha-numeric+ #.(concatenate 'string +letter+ +digit+))
+(defconstant +spaces+ #.(format nil "~c~c" #\space #\tab))
+(defconstant +newlines+ #.(format nil "~c~c~c" #\newline #\return #\linefeed))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (flet ((dispatch-re (s c n)
+           (declare (ignorable c n))
+           (let ((re (with-output-to-string (re)
+                       (loop :for c := (read-char s t nil t) :do
+                         (case c
+                           (#\/ (return))
+                           (#\\ (let ((c (read-char s t nil t)))
+                                  (princ c re)))
+                           (otherwise
+                            (princ c re)))))))
+             (compile-re re))))
+    (set-dispatch-macro-character #\# #\/ #'dispatch-re)))
+
+(defmacro with-re-match ((v match) &body body)
+  "Intern match symbols to execute a body."
+  (let (($$ (intern "$$" *package*))
+        ($1 (intern "$1" *package*))
+        ($2 (intern "$2" *package*))
+        ($3 (intern "$3" *package*))
+        ($4 (intern "$4" *package*))
+        ($5 (intern "$5" *package*))
+        ($6 (intern "$6" *package*))
+        ($7 (intern "$7" *package*))
+        ($8 (intern "$8" *package*))
+        ($9 (intern "$9" *package*)))
+    `(let ((,v ,match))
+       (when ,v
+         (destructuring-bind (,$$ &optional ,$1 ,$2 ,$3 ,$4 ,$5 ,$6 ,$7 ,$8 ,$9)
+             (cons (match-string ,v) (match-captures ,v))
+           (declare (ignorable ,$$ ,$1 ,$2 ,$3 ,$4 ,$5 ,$6 ,$7 ,$8 ,$9))
+           (values (progn ,@body) t))))))
+
+(defmacro deflexer (lexer (&rest options) &body patterns)
+  "Create a tokenizing function."
+  (declare (ignorable options))
+  (let ((lex-pos (gensym "lex-pos"))
+        (tokenize (gensym "tokenize"))
+        (source (gensym "source"))
+        (line (gensym "line"))
+        (skip-token (gensym "skip"))
+        (match (gensym "match")))
+    `(defun ,lexer (,source)
+       (let ((,lex-pos 0)
+             (,line 1))
+         #'(lambda ()
+             (when (< ,lex-pos (length ,source))
+               (block ,tokenize
+                 (tagbody
+                  ,skip-token
+                  ,@(loop :for token :in patterns :collect
+                      (destructuring-bind (re &body body)
+                          token
+                        `(with-re-match (,match (match-re ,re ,source :start ,lex-pos))
+                           (incf ,line (count #\newline (match-string ,match)))
+                           (setf ,lex-pos (match-pos-end ,match))
+                           ,(if (null body)
+                                `(go ,skip-token)
+                              `(return-from ,tokenize (progn ,@body)))))))
+                 (error (make-condition 'lex-error 
+                                        :source ,source 
+                                        :pos ,lex-pos 
+                                        :line ,line)))))))))
+
+(defparser re-parser
+  ((start exprs) $1)
+
+  ;; multiple expressions bound together
+  ((exprs compound exprs) (bind $1 $2))
+  ((exprs compound) $1)
+
+  ;; either expression a or b (a|b)
+  ((compound simple :or simple) (either $1 $3))
+  ((compound simple) $1)
+
+  ;; simple, optional, and repition (?, *, +)
+  ((simple expr :maybe) (maybe $1))
+  ((simple expr :many) (many $1))
+  ((simple expr :many1) (many1 $1))
+  ((simple expr) $1)
+
+  ;; capture expression (x)
+  ((expr :capture compound :end-capture) (capture $2))
+
+  ;; bounded expression
+  ((expr :between) 
+   (between (ch (car $1) :case-fold *case-fold*)
+            (ch (cdr $1) :case-fold *case-fold*)))
+
+  ;; single character
+  ((expr :char) (ch $1 :case-fold *case-fold*))
+
+  ;; any character and end of line/input
+  ((expr :any) (any-char :match-newline-p *multi-line*))
+  ((expr :eol) (eol :match-newline-p *multi-line*))
+
+  ;; named sets
+  ((expr :one-of) (one-of $1))
+  ((expr :none-of) (none-of $1))
+
+  ;; sets of characters ([..], [^..])
+  ((expr :set :none chars) (none-of $3 :case-fold *case-fold*))
+  ((expr :set chars) (one-of $2 :case-fold *case-fold*))
+
+  ;; character set (just a list of characters)
+  ((chars :one-of chars) (append (coerce $1 'list) $2))
+  ((chars :none-of chars) (append (coerce $1 'list) $2))
+  ((chars :char chars) (cons $1 $2))
+  ((chars :end-set) nil))
+
+(defun compile-re (pattern &key case-fold multi-line)
+  "Create a regular expression pattern match."
+  (let ((*case-fold* case-fold)
+        (*multi-line* multi-line))
+    (with-input-from-string (s pattern)
+      (flet ((next-token ()
+               (let ((c (read-char s nil nil)))
+                 (when c
+                   (case c
+                     (#\%
+                      (let ((c (read-char s)))
+                        (case c
+                          (#\b (let ((b1 (read-char s))
+                                     (b2 (read-char s)))
+                                 (values :between (cons b1 b2))))
+                          (#\s (values :one-of +spaces+))
+                          (#\S (values :none-of +spaces+))
+                          (#\n (values :one-of +newlines+))
+                          (#\N (values :none-of +newlines+))
+                          (#\a (values :one-of +letter+))
+                          (#\A (values :none-of +letter+))
+                          (#\l (values :one-of +lowercase-letter+))
+                          (#\L (values :none-of +lowercase-letter+))
+                          (#\u (values :one-of +uppercase-letter+))
+                          (#\U (values :none-of +uppercase-letter+))
+                          (#\p (values :one-of +punctuation+))
+                          (#\P (values :none-of +punctuation+))
+                          (#\w (values :one-of +alpha-numeric+))
+                          (#\W (values :none-of +alpha-numeric+))
+                          (#\d (values :one-of +digit+))
+                          (#\D (values :none-of +digit+))
+                          (#\x (values :one-of +hex-digit+))
+                          (#\X (values :none-of +hex-digit+))
+                          (#\z (values :char #\null))
+                          (otherwise
+                           (values :char c)))))
+                     (#\$ :eol)
+                     (#\. :any)
+                     (#\^ :none)
+                     (#\( :capture)
+                     (#\) :end-capture)
+                     (#\[ :set)
+                     (#\] :end-set)
+                     (#\? :maybe)
+                     (#\* :many)
+                     (#\+ :many1)
+                     (#\| :or)
+                     (otherwise
+                      (values :char c)))))))
+        (let ((expr (re-parser #'next-token)))
+          (when expr
+            (make-instance 're :pattern pattern :expression expr)))))))
+
+(defun match-re (re s &key (start 0) (end (length s)) exact)
+  "Check to see if a regexp pattern matches a string."
+  (flet ((capture (captures place)
+           (let ((start (car place))
+                 (end (cdr place)))
+             (cons (subseq s start end) captures))))
+    (with-input-from-string (source s :start start :end end)
+      (let ((st (make-instance 'lex-state :source source :captures nil)))
+        (when (funcall (re-expression re) st)
+          (let ((caps (reduce #'capture (lex-captures st) :initial-value nil))
+                (end-pos (file-position source)))
+            (when (or (not exact) (= end-pos end))
+              (make-instance 're-match
+                             :match (if exact s (subseq s start end-pos))
+                             :captures caps
+                             :start-pos start
+                             :end-pos end-pos))))))))
+
+(defun find-re (re s &key (start 0) (end (length s)) all)
+  "Find a regexp pattern match somewhere in a string."
+  (if (not all)
+      (loop :for i :from start :below end :do
+        (let ((match (match-re re s :start i :end end :exact nil)))
+          (when match
+            (return match))))
+    (loop
+     :with i := 0
+     :for match := (find-re re s :start i :end end)
+     :while match
+     :collect (prog1
+                  match
+                (setf i (match-pos-end match))))))
+
+(defun split-re (re s &key (start 0) (end (length s)) all coalesce-seps)
+  "Split a string into one or more strings by regexp pattern match."
+  (if (not all)
+      (let ((match (find-re re s :start start :end end)))
+        (if (null match)
+            s
+          (values (subseq s start (match-pos-start match))
+                  (subseq s (match-pos-end match)))))
+    (let* ((hd (list nil)) 
+           (tl hd)
+           (pos 0))
+      (flet ((push-match (&optional m)
+               (let ((s (subseq s pos (when m (match-pos-start m)))))
+                 (unless (and coalesce-seps (zerop (length s)))
+                   (setf tl (cdr (rplacd tl (list s)))))
+                 (setf pos (when m (match-pos-end m))))))
+        (loop :for sep :in (find-re re s :start start :end end :all t) :do
+          (push-match sep))
+        (push-match))
+      (cdr hd))))
+
+(defun replace-re (re with s &key (start 0) (end (length s)) all)
+  "Split a string into one or more strings by regexp pattern match."
+  (let ((matches (find-re re s :start start :end end :all all)))
+    (unless all
+      (setf matches (list matches)))
+    (with-output-to-string (rep)
+      (let ((pos 0))
+        (loop :for match :in matches :do
+          (let ((prefix (subseq s pos (match-pos-start match))))
+            (format rep "~a~a" prefix (funcall with match))
+            (setf pos (match-pos-end match))))
+        (format rep (subseq s pos))))))
+
+(defun next (st pred)
+  "Read the next character, update the pos, test against predicate."
+  (let ((c (read-char (lex-source st) nil nil)))
+    (if (funcall pred c)
+        t
+      (when c
+        (unread-char c (lex-source st))))))
+
+(defun bind (&rest ps)
+  "Bind parse combinators together to compose a new combinator."
+  #'(lambda (st)
+      (dolist (p ps t)
+        (unless (funcall p st)
+          (return nil)))))
+
+(defun capture (p)
+  "Push a capture of a combinator onto the lex state."
+  #'(lambda (st)
+      (with-slots (source captures)
+          st
+        (let ((start (file-position source)))
+          (when (funcall p st)
+            (push (cons start (file-position source)) captures))))))
+
+(defun either (p1 p2)
+  "Try one parse combinator, if it fails, try another."
+  #'(lambda (st)
+      (with-slots (source)
+          st
+        (let ((pos (file-position source)))
+          (or (funcall p1 st)
+              (progn
+                (file-position source pos)
+                (funcall p2 st)))))))
+
+(defun any-char (&key match-newline-p)
+  "Expect a non-eoi character."
+  #'(lambda (st)
+      (next st #'(lambda (x)
+                   (and x (if match-newline-p t (char/= x #\newline)))))))
+
+(defun eol (&key (match-newline-p t))
+  "End of file or line."
+  #'(lambda (st)
+      (let ((x (next st #'identity)))
+        (if (and match-newline-p (char= x #\newline))
+            t
+          (null x)))))
+
+(defun ch (c &key case-fold)
+  "Match an exact character."
+  (let ((test (if case-fold #'char-equal #'char=)))
+    #'(lambda (st)
+        (next st #'(lambda (x)
+                     (and x (funcall test x c)))))))
+
+(defun one-of (cs &key case-fold)
+  "Match any character from a set."
+  (let ((test (if case-fold #'char-equal #'char=)))
+    #'(lambda (st)
+        (next st #'(lambda (x)
+                     (and x (find x cs :test test)))))))
+
+(defun none-of (cs &key case-fold)
+  "Match any character not in a set."
+  (let ((test (if case-fold #'char-equal #'char=)))
+    #'(lambda (st)
+        (next st #'(lambda (x)
+                     (and x (not (find x cs :test test))))))))
+
+(defun maybe (p)
+  "Optionally match a parse combinator."
+  #'(lambda (st)
+      (or (funcall p st) t)))
+
+(defun many (p)
+  "Match a parse combinator zero or more times."
+  #'(lambda (st)
+      (do () ((null (funcall p st)) st))))
+
+(defun many1 (p)
+  "Match a parse combinator one or more times."
+  (bind p (many p)))
+
+(defun many-til (p term)
+  "Match a parse combinator many times until a terminal."
+  #'(lambda (st)
+      (do ()
+          ((funcall term st) t)
+        (unless (funcall p st)
+          (return nil)))))
+
+(defun between (b1 b2 &key match-newline-p)
+  "Match everything between two characters."
+  (bind b1 (many-til (any-char :match-newline-p match-newline-p) b2)))
+
+(provide "LEXER")
